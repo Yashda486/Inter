2a5d1e67f952c52dcdb98ce6bf8e6da3df86e941
Your Name <yashshahk1412@gmail.com>
2021-05-31 21:47:13 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const App=()=>{
+    return(
+        
+        <div className="container-fluid">
+        <div className="row">
+            <div className="col-lg-12 text-center">
+                <img src="../asserts/logo.png" alt="Not Found" width="27%" height="100%" /> 
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-lg-4 ">
+                <img src="1.png" alt="Not Found" width="85%" height="92%" />
+            </div>
+            <div className="col-lg-8">
+                <strong>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</strong>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <ul className="mt-3" style={{listStyleType:"disc"}} >
+                            <li style={{fontSize:"14px"}}>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for <br /> various projects across the globe to save energy.</li>
+                            <li style={{fontSize:"14px"}}>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the <br /> old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. 
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-12">
+                        <img src="2.png" alt="Not Found" height="73%" width="83%" className="yash" />
+                        <p style={{fontSize:"14px"}} className="mt-3"> Government of India has awarded the <strong> "National Energy Conservation Award 2018" </strong> Mr. G. Selvaraj, Joint Managing Director <br /> of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha and Shri. Raj Kumar Singh, Honorable <br /> Minister of State. </p>
+                    </div>
+                </div>            
+        </div>
+    </div>
+</div>    
+             
+        
+    )
+}
+export default App;
