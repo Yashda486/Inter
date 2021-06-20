c326c2495ad5bfcc0defb07cc129d73e06fabe10
Your Name <yashshahk1412@gmail.com>
2021-06-20 15:10:32 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,214 @@
 import React from 'react';
-
+import data from "./data.jpg";
 const App=()=>{
     return(
-        
-        <div className="container-fluid">
-        <div className="row">
-            <div className="col-lg-12 text-center">
-                <img src="../asserts/logo.png" alt="Not Found" width="27%" height="100%" /> 
-            </div>
-        </div>
-        <div className="row">
-            <div className="col-lg-4 ">
-                <img src="1.png" alt="Not Found" width="85%" height="92%" />
-            </div>
-            <div className="col-lg-8">
-                <strong>C.R.I. PUMPS WINS THE NATIONAL ENERGY CONSERVATION AWARD 2018 for the 4th time.</strong>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <ul className="mt-3" style={{listStyleType:"disc"}} >
-                            <li style={{fontSize:"14px"}}>C.R.I.'s energy efficient products are well recognized by various Government Institutions, as trustworthy products for <br /> various projects across the globe to save energy.</li>
-                            <li style={{fontSize:"14px"}}>C.R.I. is the highest contributor in the country for the projects of EESL (Energy Efficiency Services Limited) to replace the <br /> old inefficient pumps with 5 Star rated energy efficient smart pumps with IoT enabled control panel. 
-                            </li>
-                        </ul>
-                    </div>
+        <>
+         <div className="container">
+             <div className="row">
+                 <div className="col-lg-11 mt-4">
+                     <h2 className="text-center">Why Data Science <p style={{display:"inline-block",textDecorationStyle:"wavy",borderRadius:"100%"}}> <span style={{textDecoration:"underline wavy red",textDecorationWidth:"10px"}}>Sdot</span></p> Data <br />Science For Everyone</h2>
+                 </div>
+                 <div className="col-lg-1 mt-3">
+                     <div style={{border:"2px solid orange",height:"20px",width:"20px",backgroundColor:"orange",borderTopLeftRadius:"50%",borderTopRightRadius:"30%",borderBottomRightRadius:"60%",borderBottomLeftRadius:"20%"}}></div>
+                 </div>
+             </div>
+             <div className="row mt-5">
+                 <div className="col-lg-6">
+                     <h3 className="mt-4">Data Science sdot is one of the India's leading platforms providing data science solution</h3>
+                     <p className="mt-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim aperiam veniam ut reiciendis maiores sapiente laboriosam molestiae alias, tempore voluptates, eum ipsam!</p>
+                     <p className="mt-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim aperiam veniam ut reiciendis maiores sapiente laboriosam molestiae alias, tempore voluptates, eum ipsam!</p>
+                     <button className="mt-4" style={{borderRadius:"80px",paddingRight:"30px",color:"black",paddingLeft:"30px",paddingTop:"10px",borderColor:"black",backgroundColor:"white",paddingBottom:"10px"}}>Learn More</button>
+                 </div>
+                 <div className="col-lg-6 mt-3">
+                     <img src={data} alt="Not Found" height="380px"/>
+                 </div>
+             </div>
+         </div>
+
+<hr />
+
+         <div className="container mt-4">
+            <div className="row">
+                <div className="col-lg-12">
+                    <h2 className="text-center">Latest data science and our <br /><span style={{textDecoration:"underline wavy red",textDecorationWidth:"10px"}} >company news </span></h2>
                 </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <img src="2.png" alt="Not Found" height="73%" width="83%" className="yash" />
-                        <p style={{fontSize:"14px"}} className="mt-3"> Government of India has awarded the <strong> "National Energy Conservation Award 2018" </strong> Mr. G. Selvaraj, Joint Managing Director <br /> of C.R.I. Group received the award from Smt. Sumitra Mahajan, Speaker of Lok Sabha and Shri. Raj Kumar Singh, Honorable <br /> Minister of State. </p>
-                    </div>
-                </div>            
+            </div>
+            <div className="row mt-3">
+                <div className="col-lg-12">
+                    <p className="text-center">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem saepe, quis at cumque fugiat, <br /> amet molestias perspiciatis tenetur odit nemo itaque!</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-4 mt-5">
+                    <img src={data} alt="Not Found" height="225px" width="275px"/>
+                    <p className="mt-3" style={{color:"red"}}>Data Science</p>
+                    <h3>Data Engineer,Data <br /> Analyst Data Scientist</h3>
+                    <p>May 21,2021 . <span style={{marginLeft:"15px"}}>O comments</span></p>
+                </div>
+                <div className="col-lg-4 mt-5">
+                    <img src={data} alt="Not Found" height="225px" width="275px"/>
+                    <p className="mt-3" style={{color:"red"}}>Data Science</p>
+                    <h3>How Artificial Intelligence <br /> is changing the industry</h3>
+                    <p>May 21,2021 . <span style={{marginLeft:"15px"}}>O comments</span></p>
+                </div>
+                <div className="col-lg-4 mt-5">
+                    <img src={data} alt="Not Found" height="225px" width="275px"/>
+                    <p className="mt-3" style={{color:"red"}}>Data Science</p>
+                    <h3>What are Service Graph Connectors in ServiceNow?</h3>
+                    <p>May 21,2021 . <span style={{marginLeft:"15px"}}>O comments</span></p>
+                </div>
+            </div>
+         </div>
+
+         <hr />
+
+
+         <div className="container">
+             <div className="row mt-5">
+                 <div className="col-lg-6">
+                     <img src={data} alt="Not Found" height="450px" width="500px" style={{border:"none"}} />
+                 </div>
+                 <div className="col-lg-6 mt-3">
+                     <strong style={{color:"blue",fontSize:"20px"}}>10+ Recent Case</strong>
+                     <h2 className="mt-2" style={{fontSize:"40px"}}>#1  Data Analytics and <br /> strategy <span style={{textDecoration:"underline wavy red",textDecorationWidth:"10px"}}>projects</span></h2>
+                     <p className="mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptatem hic dolores doloribus reprehenderit nulla id! Illo recusandae fugit vel, ad doloremque placeat soluta architecto cumque porro, similique inventore harum neque incidunt sequi reprehenderit quis. Corrupti neque ipsam cum atque.</p>
+                     <button className="mt-4" style={{borderRadius:"80px",paddingRight:"35px",color:"black",paddingLeft:"35px",paddingTop:"10px",borderColor:"black",backgroundColor:"white",paddingBottom:"10px"}}>View Projects</button>
+                 </div>
+             </div>
+         </div>
+
+         <hr />
+
+
+         <div className="container-fluid">
+             <div className="row">
+                 <div className="col-lg-12">
+                
+<footer className="text-center text-lg-start bg-light text-muted">
+  
+  <section
+    className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"
+  >
+    
+    
+  </section>
+
+  
+  <section className="">
+    <div className="container text-center text-md-start mt-5">
+      
+      <div className="row mt-3">
+    
+        <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          
+          <h6 className="text-uppercase fw-bold mb-4">
+            <span style={{fontFamily:"italic",fontSize:"35px",color:"darkblue"}}>S</span><strong style={{textTransform:"lowercase",fontSize:"25px",color:"black"}}><span style={{textDecorationStyle:"solid",textDecorationLine:"underline",textDecorationColor:"red"}}>d</span>ot</strong>
+          </h6>
+          <p>
+           821 11h Street,Office 372 <br />Berlin, De 1524 GA
+          </p>
+          <a href="#!" class="me-4 text-reset">
+        <i class="fab fa-facebook-f active"></i>
+      </a>
+      <a href="#!" class="me-4 text-reset">
+      <i class="fab fa-linkedin-in"></i>
+      </a>
+      <a href="#!" class="me-4 text-reset">
+        <i class="fab fa-twitter"></i>
+      </a>
+      <a href="#!" class="me-4 text-reset">
+        <i class="fab fa-instagram"></i>
+      </a>
         </div>
-    </div>
-</div>    
-             
+    
+        <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
         
+          <h6 className="text-uppercase fw-bold mb-4" style={{color:"black"}}>
+            Links
+          </h6>
+          <p>
+            <a href="#!" className="text-reset" style={{textDecoration:"none"}}>About us</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset" style={{textDecoration:"none"}}>Our News</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset" style={{textDecoration:"none"}}>Our Portfolio</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset" style={{textDecoration:"none"}}>Solution</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset" style={{textDecoration:"none"}}>Contacts</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset" style={{textDecoration:"none"}}>Our Careers</a>
+          </p>
+        </div>
+
+
+        
+        <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+        
+          <h6 className="text-uppercase fw-bold mb-4" style={{color:"black"}}>
+            Services
+          </h6>
+          <p>
+            <a href="#!" className="text-reset"style={{textDecoration:"none"}}>Technology</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset"style={{textDecoration:"none"}}>Healthcare</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset"style={{textDecoration:"none"}}>Data Science</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset"style={{textDecoration:"none"}}>Applied math</a>
+          </p>
+          <p>
+            <a href="#!" className="text-reset"style={{textDecoration:"none"}}>Data analysis</a>
+          </p>
+        </div>
+
+    
+        <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+        
+          <h6 className="text-uppercase fw-bold mb-4" style={{color:"black"}}>
+            Get in Touch
+          </h6>
+          <p>Sinfosdot@gmail.com</p>
+          <p>
+            +88 016 826 48 11
+          </p>
+          <br />
+          <p><strong>Sign up for Newsletter</strong></p>
+          <div class="input-group mb-3">
+  <input type="email" class="form-control" placeholder="Email address" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+  <div class="input-group-append">
+    <button class="btn btn-outline-secondary" type="button" style={{backgroundColor:"blue",color:"white"}}>Subscribe</button>
+  </div>
+</div>
+            </div>
+    
+      </div>
+      
+    </div>
+  </section>
+
+
+
+  <div className="p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.05)"}}>
+    Copyright 2021 All Right Reserved
+  </div>
+  
+</footer>
+                 </div>
+             </div>
+         </div>
+        </>
+
+     
     )
+    
 }
 export default App;
